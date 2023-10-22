492538b1dffe916b69758fa6f900c4625f6de2a2
KareemWebDeveloper <kemoayman2015@gmail.com>
2023-10-22 22:25:50 +0300

    Cities Feature

---
+++ src/views/Cities/CityCreateView.vue
@@ -0,0 +1,12 @@
+ 
+<script setup lang="ts">
+import AppForm from 'v-dashkit/form/AppForm';
+import { useI18n } from 'vue-i18n';
+import { getCityFormProps } from './CitiesForm'
+const { t } = useI18n()
+
+const formProps = await getCityFormProps(t, 'create')
+</script>
+<template>
+    <app-form :formProps="formProps" />
+</template>
---
+++ src/views/Cities/CityFindView.vue
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+       <h1>City Find</h1>
+    </div>
+</template>
