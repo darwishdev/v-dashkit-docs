ba2559a61c084b90b04aabfa6394d957bed23882
KareemWebDeveloper <kemoayman2015@gmail.com>
2023-10-29 21:41:20 +0200

    units create / update

---
+++ src/views/Units/UnitCreate.vue
@@ -0,0 +1,12 @@
+ 
+<script setup lang="ts">
+import AppForm from 'v-dashkit/form/AppForm';
+import { useI18n } from 'vue-i18n';
+import { getUnitFormProps } from './UnitForm'
+const { t } = useI18n()
+
+const formProps = await getUnitFormProps(t, 'create')
+</script>
+<template>
+    <app-form :formProps="formProps" />
+</template>
